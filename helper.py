b18a0bceb0175e90dd76335d2736a22434a55500
immodi <ay727467@gmail.com>
2023-11-20 18:13:45 +0200

    commit

---
+++ helper.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from zipfile import ZipFile
+from pathlib import Path
+
+
+def get_image(name: str, url: str, dir: str):
+    path = Path(dir, name)
+    with open(f'{path}.jpg', 'wb') as handle:
+        response = requests.get(url, stream=True)
+
+        if not response.ok:
+            print(response)
+            return
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+            handle.write(block)
+
+
+def make_dir(name: str):
+    try:  
+        os.mkdir(name)  
+    except OSError as error:  
+        return
+    
+    
+def zip_dir(dir_path):
+    with ZipFile(f"{dir_path}.zip", 'w') as z:
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                z.write(os.path.join(root, file))
+            for directory in dirs:
+                z.write(os.path.join(root, directory))
---
+++ main.py
@@ -0,0 +1,65 @@
+import sys
+import requests
+import helper
+from scrapy.http import HtmlResponse
+from scrapy.selector import Selector
+from pathlib import Path
+
+
+class WebtoonScrapper():
+    def __init__(self, series_name: str, link: str, starting_chapter: int, total_chapters: int, target_website: str):
+        self.link = link
+        self.starting_chapter = starting_chapter
+        self.total_chapters = total_chapters
+        self.series_name = series_name
+        self.target_website = target_website
+        
+    def execute(self):
+        selectors = {
+            # webtoon_source: (parent_element, next_chapter)
+            "hiperdex": ('page-break', 'next_page'),
+            "manga18fx": ('page-break', 'navi-change-chapter-btn-next'),
+            "toonily": ('page-break', 'next_page'),
+        } 
+        
+        helper.make_dir(self.series_name)
+
+        for current_chapter in range(self.total_chapters):
+            try:
+                request = requests.get(self.link)          
+                response = HtmlResponse(url=self.link, body=request.text, encoding="utf-8")
+            
+                images_url = Selector(response=response).response.css(f'.{selectors[self.target_website][0]} img::attr(src)').extract()
+                self.save_all_images(images_url, self.starting_chapter)
+                
+                if self.target_website == "manga18fx":
+                    self.link = "https://manga18fx.com/" + self.link
+                    self.link = "-".join(self.link.split("-")[0:-1]) + f"-{self.starting_chapter+1}"
+                    
+                try: 
+                    self.link = Selector(response=response).css(f".{selectors[self.target_website][1]}::attr(href)").extract()[0]
+                except IndexError:
+                    break
+                    
+                print(f"next_url => {self.link}")
+                self.starting_chapter += 1
+                
+            except Exception as e:
+                print(e)
+                continue
+
+        helper.zip_dir(self.series_name)
+
+    def save_all_images(self, images_url_arr: list, chapter_num: int):
+        # current_dir = f"{self.series_name}\\{chapter_num}"
+        current_dir = Path(self.series_name, chapter_num)
+        helper.make_dir(current_dir)
+        
+        for i in range(images_url_arr.__len__()):
+            helper.get_image(i+1, images_url_arr[i], current_dir)
+            
+
+
+webtoon = WebtoonScrapper(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), sys.argv[5])
+webtoon.execute()
+
